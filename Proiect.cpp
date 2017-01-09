d0d9b18fcd183cac7b25fc7aaef4cf8694a861e5
alexzahariadanut <alexzahariadanut@users.noreply.github.com>
2017-01-09 15:41:35 +0200

    Update Proiect.cpp

--- Proiect.cpp
+++ Proiect.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include "windows.h"
+#include"Pro.h"
 
+using namespace std;
+
+int main()
+{
+    int x,y;
+    system("color 0B");
+
+    while (gameEnd==false)
+    {
+        system("cls");
+        for(int i=0; i<24; i++)
+        {
+
+
+            cout<<Map1[i] << endl;
+
+        }
+        cout<<"Health"<<yourHealthT1<<"/"<<maxHealth<<"          "<<"Health"<<yourHealthT2<<"/"<<maxHealth;
